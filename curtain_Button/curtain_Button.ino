47ff7c1c649f57f178ee8f68748a19f15d0a8174
joannqn <joannangocquyen@gmail.com>
2026-04-23 10:19:18 -0500

    Update curtain and light button sketches

--- curtain_Button/curtain_Button.ino
+++ curtain_Button/curtain_Button.ino
@@ -1,9 +1,87 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <mcp_can.h>
+#include <mcp_can_dfs.h>
 
+MCP_CAN CAN(9);   // CS pin for MCP2515
+
+// ---------------------------
+// Pin setup
+// ---------------------------
+const int OPEN_BUTTON_PIN  = 2;
+const int CLOSE_BUTTON_PIN = 3;
+
+// ---------------------------
+// CAN setup
+// ---------------------------
+const unsigned int CURTAIN_CAN_ID = 0x300;
+
+// Data values for curtain commands
+const byte CURTAIN_OPEN_CMD  = 0x01;
+const byte CURTAIN_CLOSE_CMD = 0x00;
+
+// For button state tracking
+int lastOpenButtonState  = HIGH;
+int lastCloseButtonState = HIGH;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(OPEN_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(CLOSE_BUTTON_PIN, INPUT_PULLUP);
+
+    // Initialize CAN
+    while (CAN_OK != CAN.begin(CAN_125KBPS))
+    {
+        Serial.println("CAN init fail, retrying...");
+        delay(100);
+    }
+
+    Serial.println("CAN init ok!");
+    Serial.println("Curtain sender ready.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    int openButtonState  = digitalRead(OPEN_BUTTON_PIN);
+    int closeButtonState = digitalRead(CLOSE_BUTTON_PIN);
 
+    // Detect button press for OPEN
+    if (openButtonState == LOW && lastOpenButtonState == HIGH)
+    {
+        sendCurtainCommand(CURTAIN_OPEN_CMD);
+        Serial.println("Sent: CURTAIN OPEN");
+        delay(200);  // simple debounce
+    }
+
+    // Detect button press for CLOSE
+    if (closeButtonState == LOW && lastCloseButtonState == HIGH)
+    {
+        sendCurtainCommand(CURTAIN_CLOSE_CMD);
+        Serial.println("Sent: CURTAIN CLOSE");
+        delay(200);  // simple debounce
+    }
+
+    lastOpenButtonState = openButtonState;
+    lastCloseButtonState = closeButtonState;
+}
+
+void sendCurtainCommand(byte command)
+{
+    byte data[1];
+    data[0] = command;
+
+    byte sendResult = CAN.sendMsgBuf(CURTAIN_CAN_ID, 0, 1, data);
+
+    if (sendResult == CAN_OK)
+    {
+        Serial.print("Message sent on CAN ID 0x");
+        Serial.print(CURTAIN_CAN_ID, HEX);
+        Serial.print(" | Data: ");
+        Serial.println(data[0], HEX);
+    }
+    else
+    {
+        Serial.println("Error sending message.");
+    }
 }
--- light_button/light_button.ino
+++ light_button/light_button.ino
@@ -1,9 +1,84 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <mcp_can.h>
+#include <mcp_can_dfs.h>
 
+MCP_CAN CAN(9);   // CS pin for MCP2515
+
+// ---------------------------
+// Pin setup
+// ---------------------------
+const int LIGHT_ON_BUTTON_PIN  = 2;
+const int LIGHT_OFF_BUTTON_PIN = 3;
+
+// ---------------------------
+// CAN setup
+// ---------------------------
+const unsigned int LIGHT_CAN_ID = 0x100;
+
+// Data values for light commands
+const byte LIGHT_ON_CMD  = 0x01;
+const byte LIGHT_OFF_CMD = 0x00;
+
+// For button state tracking
+int lastLightOnButtonState  = HIGH;
+int lastLightOffButtonState = HIGH;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(LIGHT_ON_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LIGHT_OFF_BUTTON_PIN, INPUT_PULLUP);
+
+    while (CAN_OK != CAN.begin(CAN_125KBPS))
+    {
+        Serial.println("CAN init fail, retrying...");
+        delay(100);
+    }
+
+    Serial.println("CAN init ok!");
+    Serial.println("Light sender ready.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    int lightOnButtonState  = digitalRead(LIGHT_ON_BUTTON_PIN);
+    int lightOffButtonState = digitalRead(LIGHT_OFF_BUTTON_PIN);
 
+    if (lightOnButtonState == LOW && lastLightOnButtonState == HIGH)
+    {
+        sendLightCommand(LIGHT_ON_CMD);
+        Serial.println("Sent: LIGHT ON");
+        delay(200);
+    }
+
+    if (lightOffButtonState == LOW && lastLightOffButtonState == HIGH)
+    {
+        sendLightCommand(LIGHT_OFF_CMD);
+        Serial.println("Sent: LIGHT OFF");
+        delay(200);
+    }
+
+    lastLightOnButtonState = lightOnButtonState;
+    lastLightOffButtonState = lightOffButtonState;
+}
+
+void sendLightCommand(byte command)
+{
+    byte data[1];
+    data[0] = command;
+
+    byte sendResult = CAN.sendMsgBuf(LIGHT_CAN_ID, 0, 1, data);
+
+    if (sendResult == CAN_OK)
+    {
+        Serial.print("Message sent on CAN ID 0x");
+        Serial.print(LIGHT_CAN_ID, HEX);
+        Serial.print(" | Data: ");
+        Serial.println(data[0], HEX);
+    }
+    else
+    {
+        Serial.println("Error sending message.");
+    }
 }
