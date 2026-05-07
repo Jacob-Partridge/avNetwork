c44f9816140bffa8f223fb76ded72916bd291193
Jacob <jepartridge@shockers.wichita.edu>
2026-05-06 21:52:02 -0500

    commit

--- JoReceiver/JoReceiver.ino
+++ JoReceiver/JoReceiver.ino
@@ -1,101 +1,76 @@
 #include <SPI.h>
 #include <mcp_can.h>
-#include <mcp_can_dfs.h>
+#include <Servo.h>
 
-MCP_CAN CAN(9);   // CS pin for MCP2515
+MCP_CAN CAN(9); 
 
-// Simplified CAN IDs
-const unsigned int EMERGENCY_CAN_ID = 0x00;
-const unsigned int LIGHT_CAN_ID     = 0x01;
-const unsigned int FAN_CAN_ID       = 0x02;
-const unsigned int CURTAIN_CAN_ID   = 0x03;
+// Pins
+const int EMS_LED     = 6;
+const int LIGHT       = 10;
+const int CURTAIN     = 11;
+const int FAN_PIN     = 3;
 
-const unsigned int EMS_LED          = 9;
-const unsigned int LIGHT            = 10;
-const unsigned int CURTAIN          = 11;
-const unsigned int FAN              = 6;
-void setup()
-{
+// State Tracking
+bool fanOn = false; 
+int servoPos = 0;
+int direction = 1;
+unsigned long lastMoveTime = 0;
+const int fanSpeed = 15; 
+
+Servo myServo;
+
+void setup() {
     Serial.begin(115200);
-    pinMode(6, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    while (CAN_OK != CAN.begin(CAN_125KBPS))
-    {
+    pinMode(EMS_LED, OUTPUT);
+    pinMode(LIGHT, OUTPUT);
+    pinMode(CURTAIN, OUTPUT);
+    myServo.attach(FAN_PIN);
+    
+    while (CAN_OK != CAN.begin(CAN_125KBPS)) {
         Serial.println("CAN init fail, retrying...");
         delay(100);
     }
+    Serial.println("System Ready.");
+}
 
-    Serial.println("CAN init ok!");
-    Serial.println("Receiver ready.");
-    }
+void loop() {
 
-void loop()
-{
-    if (CAN_MSGAVAIL == CAN.checkReceive())
-    {
+    // --- 2. COMMS CHECK: CAN Bus ---
+    if (CAN_MSGAVAIL == CAN.checkReceive()) {
         unsigned char len = 0;
         unsigned char buf[8];
-
         CAN.readMsgBuf(&len, buf);
         unsigned int rxId = CAN.getCanId();
 
-        Serial.print("Received CAN ID: ");
-        Serial.print(rxId);
-        Serial.print(" | Data: ");
-
-        for (int i = 0; i < len; i++)
-        {
-            Serial.print(buf[i]);
-            Serial.print(" ");
-        }
-        Serial.println();
-
-        if (rxId == EMERGENCY_CAN_ID)
-        {
-            if (buf[0] == 0x01)
-                digitalWrite(EMS_LED, HIGH);
-            
-            else if (buf[0] == 0x00)
-                digitalWrite(EMS_LED, LOW);
-        }
-
-        else if (rxId == LIGHT_CAN_ID)
-        {
-            if (len > 0 && buf[0] == 0x01)
-                digitalWrite(LIGHT, HIGH);
-
-            else if (len > 0 && buf[0] == 0x00)
-                digitalWrite(LIGHT, LOW);
-        }
-        else if (rxId == FAN_CAN_ID)
-        {
-            if (len > 0 && buf[0] == 0x01)
-            {
-                Serial.println("FAN ON");
-            }
-            else if (len > 0 && buf[0] == 0x00)
-            {
-                Serial.println("FAN OFF");
+        if (rxId == 0x00) { // Emergency
+            digitalWrite(EMS_LED, buf[0]);
+        } 
+        else if (rxId == 0x01) { // Light
+            digitalWrite(LIGHT, buf[0]);
+        } 
+        else if (rxId == 0x02) { // Fan
+            if (buf[0] == 0x01) fanOn = true;
+            else if (buf[0] == 0x00) {
+                fanOn = false; // Only turn off if temp is also safe
             }
         }
-        else if (rxId == CURTAIN_CAN_ID)
-        {
-            if (len > 0 && buf[0] == 0x01)
-            {
-                digitalWrite(CURTAIN, HIGH);
-            }
-            else if (len > 0 && buf[0] == 0x00)
-            {
-                digitalWrite(CURTAIN, LOW);
-            }
+        else if (rxId == 0x03) { // Curtain
+            digitalWrite(CURTAIN, LOW);
         }
-        else
-        {
-            Serial.println("Unknown message received.");
-        }
+    }
 
-        Serial.println();
+    // --- 3. EXECUTION: Fan Movement ---
+    if (fanOn) {
+        if (millis() - lastMoveTime > fanSpeed) {
+            lastMoveTime = millis();
+            servoPos += direction;
+            myServo.write(servoPos);
+        }
+    } else {
+        // Optional: Return fan to parked position when off
+        if (servoPos != 0) {
+            servoPos = 0;
+            myServo.write(servoPos);
+        }
     }
 }
